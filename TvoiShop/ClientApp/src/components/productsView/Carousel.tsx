b0e87556c84b003ccbb63c51c65c40840b5fe6b2
DemianDD <thvarichdemyan@icloud.com>
2022-12-03 18:10:46 +0200

    I add horizontal scroll on buttons: left/right

--- TvoiShop/ClientApp/src/components/productsView/Carousel.tsx
+++ TvoiShop/ClientApp/src/components/productsView/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Carousel from 'better-react-carousel'
 import { useNavigate } from 'react-router-dom';
+import {ReactComponent as RightIcon} from "../../images/rightIcon.svg";
+import {ReactComponent as LeftIcon} from "../../images/leftIcon.svg";
 
 interface IProps {
     products: IProduct[];
@@ -11,26 +12,42 @@ export const CarouselGallery = (props: IProps) => {
     const productItems = props.products;
     const navigate = useNavigate();
 
-    const viewProducts = productItems.map((p, index) => {
+    const sliderLeft = () => {
+      var slider = document.getElementById('slider');
+      if (slider) slider.scrollLeft = slider?.scrollLeft - 500;
+    };
+    
+    const sliderRight = () => {
+      var slider = document.getElementById('slider');
+      if (slider )slider.scrollLeft = slider?.scrollLeft + 500;
+    };
+
+    const viewCarousel = productItems.map((p, index) => {
         return(
-                <div className='productCard' key={index} onClick={() => navigate(`/productInfo/${p.id}`)}>
+          <div className='productCard' key={index} onClick={() => navigate(`/productInfo/${p.id}`)}>
 
-                    <div className="productImagePos">
-                      <div className='ProductIconPos'></div>
-                      <img className='productImage' src={p.image}/>
-                    </div>
+              <div className="productImagePos">
+                <div className='ProductIconPos'></div>
+                <img className='productImage' src={p.image}/>
+              </div>
 
-                    <div className='productLabel'>
-                      <div>{p.labelName}</div>
-                      <div className='productPrice'>{p.price} UAH</div>
-                    </div>
-                </div>
+              <div className='productLabel'>
+                <div>{p.labelName}</div>
+                <div className='productPrice'>{p.price} UAH</div>
+              </div>
+          </div>
         )
       })
 
   return (
-    <div className='productViewCarouselParent'> 
-          {viewProducts}
+    <div>
+      <div className='productViewCarouselParent'> 
+      <button onClick={sliderLeft} className ='btnCarouselLeft'> <LeftIcon/> </button>
+      <button onClick={sliderRight} className ='btnCarouselRight'> <RightIcon/> </button>
+        <div id='slider' className='carousel'>
+          {viewCarousel}
+        </div>
+      </div>
     </div>
-  )
+    )
 }
